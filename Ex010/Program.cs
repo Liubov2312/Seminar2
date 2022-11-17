d87b27a56da390a122ff75beff2a5893e7cf7c08
Liubov.V.B <griark0611@gmail.com>
2022-11-17 16:17:42 +0300

    add Ex010

---
+++ Ex010/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int secondCifra = num / 10 % 10;
+Console.WriteLine(secondCifra);
