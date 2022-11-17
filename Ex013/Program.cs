fa1c0997d65846893fab385b6ca40b78e46d64a5
Liubov.V.B <griark0611@gmail.com>
2022-11-17 16:53:49 +0300

    add Ex013

---
+++ Ex013/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 0) num = -num;
+
+while (num / 100 > 9) num = num / 10;
+
+if (num / 100 > 0)
+    {
+    int thirdCifra = num % 10;
+    Console.WriteLine($"третья цифра: {thirdCifra}");
+    }
+else
+    Console.WriteLine("третьей цифры нет");
